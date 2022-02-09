d6e9a2dcaf6876de0b6bfdc09aee62d4d92d328c
brimatteng <btenggren20@gmail.com>
2022-02-08 20:57:50 -0500

    Add a static pages controller!
    Why this change is being made:
    - We want to be able to have a few static landing pages probably with some other
      nonsense we are going to want. This is going to do that for us

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#show", page: "home"
+
+  # Handle Static pages
+  get "/pages/:page" => "pages#show"
 end
